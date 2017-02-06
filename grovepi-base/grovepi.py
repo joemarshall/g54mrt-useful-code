c9a6769b5d5e48893b1a2a9e80fbe569b1c57d15
Joe Marshall <joe.marshall@nottingham.ac.uk>
2017-02-06 14:36:03 +0000

    updates for latest version of grovepi board

--- grovepi-base/grovepi.py
+++ grovepi-base/grovepi.py
@@ -20,6 +20,12 @@ import time
 import math
 import RPi.GPIO as GPIO
 import struct
+import sys
+
+if sys.version_info<(3,0):
+	p_version=2
+else:
+	p_version=3
 
 rev = GPIO.RPI_REVISION
 if rev == 2 or rev == 3:
@@ -37,52 +43,74 @@ aRead_cmd=[3]   #analogRead() command format header
 aWrite_cmd=[4]  #analogWrite() command format header
 pMode_cmd=[5]   #pinMode() command format header
 uRead_cmd=[7]   #Ultrasonic read
+# Get firmware version
+version_cmd = [8]
 acc_xyz_cmd=[20]    #Accelerometer (+/- 1.5g) read
 rtc_getTime_cmd=[30]    #RTC get time
 dht_temp_cmd=[40]   #DHT Pro sensor temperature
 
+unused = 0
+retries = 10
+
 #Function declarations of the various functions used for encoding and sending data from RPi to Arduino
-
-
-def write_i2c_block(address,block):
+# Write I2C block
+def write_i2c_block(address, block):
     """ Write I2C block, used internally only """
-    return bus.write_i2c_block_data(address,1,block)
+	for i in range(retries):
+		try:
+			return bus.write_i2c_block_data(address, 1, block)
+		except IOError:
+			if debug:
+				print ("IOError")
+	return -1
 
-
+# Read I2C byte
 def read_i2c_byte(address):
     """ Read I2C byte, used internally only """
-    return bus.read_byte(address)
+	for i in range(retries):
+		try:
+			return bus.read_byte(address)
+		except IOError:
+			if debug:
+				print ("IOError")
+	return -1
 
 
+# Read I2C block
 def read_i2c_block(address):
     """ Read I2C block, used internally only """
-    return bus.read_i2c_block_data(address,1)
+	for i in range(retries):
+		try:
+			return bus.read_i2c_block_data(address, 1)
+		except IOError:
+			if debug:
+				print ("IOError")
+	return -1
 
 def digitalRead(pin):
     """ Arduino Digital Read from digital pin <pin>"""
-    for retries in range(5):
-        try:
-            write_i2c_block(address,dRead_cmd+[pin,0,0])
-            time.sleep(.01)
-            n=read_i2c_byte(address)
-            return n
-        except IOError:
-            time.sleep(.01)
-    print("IOError digital read")
-    return -1
+	write_i2c_block(address, dRead_cmd + [pin, unused, unused])
+	# time.sleep(.1)
+	n = read_i2c_byte(address)
+	return n
 
-def digitalWrite(pin,value):
+# Arduino Digital Write
+def digitalWrite(pin, value):
     """ Arduino Digital write to digital pin <pin>"""
-    for retries in range(5):
-        try:
-            write_i2c_block(address,dWrite_cmd+[pin,value,0])
-            return 1
-        except IOError:
-            time.sleep(0.01)
-    return -1
+	write_i2c_block(address, dWrite_cmd + [pin, value, unused])
+	return 1
+    
+def version():
+    """ Read the firmware version from the GrovePI board """
+	write_i2c_block(address, version_cmd + [unused, unused, unused])
+	time.sleep(.1)
+	read_i2c_byte(address)
+	number = read_i2c_block(address)
+	return "%s.%s.%s" % (number[1], number[2], number[3])
 
+    
 #Setting Up Pin mode on Arduino
-def pinMode(pin,mode):
+def pinMode(pin, mode):
     """ Arduino set pin mode for digital pin <pin>
     
         Don't mess with this - if you set a pin as output, and connect a sensor to it, bad things can happen.
@@ -93,107 +121,76 @@ def pinMode(pin,mode):
             mode:
                 One of "OUTPUT" or "INPUT", as to whether this pin is an output or an input.
     """
-    try:
-        if mode == "OUTPUT":
-            write_i2c_block(address,pMode_cmd+[pin,1,0])
-        elif mode == "INPUT":
-            write_i2c_block(address,pMode_cmd+[pin,0,0])
-        return 1
-    except IOError:
-        print("IOError")
-        return -1
+	if mode == "OUTPUT":
+		write_i2c_block(address, pMode_cmd + [pin, 1, unused])
+	elif mode == "INPUT":
+		write_i2c_block(address, pMode_cmd + [pin, 0, unused])
+	return 1
         
-#Read analog value from Pin
+
+# Read analog value from Pin
 def analogRead(pin):
     """ Read analog value from analog pin <pin> """
-    for retries in range(5):
-        try:
-            bus.write_i2c_block_data(address,1,aRead_cmd+[pin,0,0])
-            time.sleep(.01)
-            bus.read_byte(address)
-            number = bus.read_i2c_block_data(address,1)
-            return number[1]*256+number[2]
-        except IOError:
-            pass
-    print("IOError analog read")
-    return -1
+	write_i2c_block(address, aRead_cmd + [pin, unused, unused])
+	read_i2c_byte(address)
+	number = read_i2c_block(address)
+	return number[1] * 256 + number[2]
 
-def analogWrite(pin,value):
+
+# Write PWM
+def analogWrite(pin, value):
     """ Write PWM on digital pin <pin> """
-        
-    try:
-        write_i2c_block(address,aWrite_cmd+[pin,value,0])
-        return 1
-    except IOError:
-        print("IOError")
-        return -1
-
-def temp(pin):
+	write_i2c_block(address, aWrite_cmd + [pin, value, unused])
+	return 1
+                     
+def temp(pin,model = '1.0'):
     """ Read temp from Grove Temp Sensor """
-    try:
-        a=analogRead(pin)
-        resistance=(float)(1023-a)*10000/a
-        t=(float)(1/(math.log(resistance/10000)/3975+1/298.15)-273.15) 
-        return t
-    except IOError:
-        print("IOError")
-        return -1
-
+	# each of the sensor revisions use different thermistors, each with their own B value constant
+	if model == '1.2':
+		bValue = 4250  # sensor v1.2 uses thermistor ??? (assuming NCP18WF104F03RC until SeeedStudio clarifies)
+	elif model == '1.1':
+		bValue = 4250  # sensor v1.1 uses thermistor NCP18WF104F03RC
+	else:
+		bValue = 3975  # sensor v1.0 uses thermistor TTC3A103*39H
+	a = analogRead(pin)
+	resistance = (float)(1023 - a) * 10000 / a
+	t = (float)(1 / (math.log(resistance / 10000) / bValue + 1 / 298.15) - 273.15)
+	return t
+    
 def ultrasonicRead(pin):
     """ Read value from Grove Ultrasonic sensor """
-    for retries in range(5):
-        try:
-            write_i2c_block(address,uRead_cmd+[pin,0,0])
-            time.sleep(.06)
-            read_i2c_byte(address)
-            number = read_i2c_block(address) 
-            return (number[1]*256+number[2])
-        except IOError:
-            pass
-    print("IOError ultrasonic read")
-    return -1
-        
+	write_i2c_block(address, uRead_cmd + [pin, unused, unused])
+	time.sleep(.06)	#firmware has a time of 50ms so wait for more than that
+	read_i2c_byte(address)
+	number = read_i2c_block(address)
+	return (number[1] * 256 + number[2])
+
+    
 def ultrasonicReadBegin(pin):
     """ Start to read value from Grove Ultrasonic sensor - you can't do other grovepi things before doing ultrasonicReadFinish, but you can read accelerometer, nfc etc. """
-    for retries in range(5):
-        try:
-            write_i2c_block(address,uRead_cmd+[pin,0,0])
-            return 0
-        except IOError:
-            pass
-    print("IOError ultrasonic read")
-    return -1
+    write_i2c_block(address,uRead_cmd+[pin,0,0])
+    return 0
 
 def ultrasonicReadFinish(pin):
     """ Finish reading value from Grove Ultrasonic sensor - you can't do other grovepi things between ultrasonicReadBegin and ultrasonicReadFinish, but you can read accelerometer, nfc etc. """
-    for retries in range(5):
-        try:
-            read_i2c_byte(address)
-            number = read_i2c_block(address) 
-            return (number[1]*256+number[2])
-        except IOError:
-            pass
-    print("IOError ultrasonic read")
-    return -1         
+    read_i2c_byte(address)
+    number = read_i2c_block(address) 
+    return (number[1]*256+number[2])
         
         
 def acc_xyz():
     """ Read Grove Accelerometer (+/- 1.5g) XYZ value """
-    try:
-        write_i2c_block(address,acc_xyz_cmd+[0,0,0])
-        time.sleep(.1)
-        read_i2c_byte(address)
-        number = read_i2c_block(address)
-        if number[1]>32:
-            number[1]=-(number[1]-224)
-        if number[2]>32:
-            number[2]=-(number[2]-224)
-        if number[3]>32:
-            number[3]=-(number[3]-224)
-        return (number[1],number[2],number[3])
-    except IOError:
-        print("IOError")
-        return None
+    write_i2c_block(address,acc_xyz_cmd+[0,0,0])
+    time.sleep(.1)
+    read_i2c_byte(address)
+    number = read_i2c_block(address)
+    if number[1]>32:
+        number[1]=-(number[1]-224)
+    if number[2]>32:
+        number[2]=-(number[2]-224)
+    if number[3]>32:
+        number[3]=-(number[3]-224)
+    return (number[1],number[2],number[3])
    
 
 def rtc_getTime():
@@ -209,50 +206,44 @@ def dht(pin,module_type):
     """ Read and return temperature and humidity from Grove DHT Pro """
     for retries in range(5):
         try:
-            write_i2c_block(address,dht_temp_cmd+[pin,module_type,0])
+            write_i2c_block(address, dht_temp_cmd + [pin, module_type, unused])
 
-            #Delay necessary for proper reading fron DHT sensor
-            time.sleep(.6) 
+            # Delay necessary for proper reading fron DHT sensor
+            # time.sleep(.6)
             try:
                 read_i2c_byte(address)
                 number = read_i2c_block(address)
-                if number==-1:
-                    return -1
-            except (TypeError,IndexError):
-                return -1
-            #data returned in IEEE format as a float in 4 bytes 
-            f=0
+                # time.sleep(.1)
+                if number == -1:
+                    return [-1,-1]
+            except (TypeError, IndexError):
+                return [-1,-1]
+            # data returned in IEEE format as a float in 4 bytes
+            
+            if p_version==2:
+                h=''
+                for element in (number[1:5]):
+                    h+=chr(element)
+                    
+                t_val=struct.unpack('f', h)
+                t = round(t_val[0], 2)
 
-            for element in reversed(number[1:5]):   #data is reversed 
-                hex_val=hex(element)    #Converted to hex
-                #print hex_val
-                try:
-                    h_val=hex_val[2]+hex_val[3]
-                except IndexError:
-                    h_val='0'+hex_val[2]
-                if f==0:    #Convert to char array
-                    h=h_val
-                    f=1
-                else:
-                    h=h+h_val
-            t=round(struct.unpack('!f', h.decode('hex'))[0],2)#convert the temp back to float
-
-            h=''
-            for element in reversed(number[5:9]):   #data is reversed 
-                hex_val=hex(element)    #Converted to hex
-                #print hex_val
-                try:
-                    h_val=hex_val[2]+hex_val[3]
-                except IndexError:
-                    h_val='0'+hex_val[2]
-                if f==0:    #Convert to char array
-                    h=h_val
-                    f=1
-                else:
-                    h=h+h_val
-            hum=round(struct.unpack('!f', h.decode('hex'))[0],2)#convert back to float
-            return [t,hum]
+                h = ''
+                for element in (number[5:9]):
+                    h+=chr(element)
+                
+                hum_val=struct.unpack('f',h)
+                hum = round(hum_val[0], 2)
+            else:
+                t_val=bytearray(number[1:5])
+                h_val=bytearray(number[5:9])
+                t=round(struct.unpack('f',t_val)[0],2)
+                hum=round(struct.unpack('f',h_val)[0],2)
+            if t > -100.0 and t <150.0 and hum >= 0.0 and hum<=100.0:
+                return [t, hum]
+            else:
+                return [float('nan'),float('nan')]        
         except IOError:
             pass
-    print "Error, couldn't read DHT 5 times"
-    return -1
+    print ("Error, couldn't read DHT 5 times")
+    return [-1,1]
