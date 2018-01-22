34916e635c1e84ecc131f38ba9b8971c0f2852e0
Joe Marshall <joe.marshall@nottingham.ac.uk>
2018-01-22 11:40:30 +0000

    even more robust updating

--- startup-scripts/afterUpdate.sh
+++ startup-scripts/afterUpdate.sh
@@ -3,7 +3,11 @@
 # else you need an extra reboot
 echo "Git changed - copying code across"
 sudo chown pi.pi -R /home/pi/g54mrt-useful-code
-sudo cp /home/pi/g54mrt-useful-code/startup-scripts/rc.local /etc/rc.local
-sudo cp /home/pi/g54mrt-useful-code/startup-scripts/showIP.py /home/pi/showIP.py
+if [ -s "/home/pi/g54mrt-useful-code/startup-scripts/checkUpdate.sh" ] 
+then
+    sudo cp /home/pi/g54mrt-useful-code/startup-scripts/checkUpdate.sh /home/pi/checkUpdate.sh
+    sudo cp /home/pi/g54mrt-useful-code/startup-scripts/rc.local /etc/rc.local
+    sudo cp /home/pi/g54mrt-useful-code/startup-scripts/showIP.py /home/pi/showIP.py
+fi
 cd /home/ubi
 sudo -u ubi bash ./getlatest.sh
