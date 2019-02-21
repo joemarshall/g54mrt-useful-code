910571572d051a4e71ba9815f6750372944f3b5b
Joe Marshall <joe.marshall@nottingham.ac.uk>
2019-02-21 12:58:00 +0000

    simpler nfc reading code

--- grovepi-base/grovenfcreader.py
+++ grovepi-base/grovenfcreader.py
@@ -9,50 +9,62 @@
 
 
 """
+USE_NFCPY=False
+
+if USE_NFCPY:
+
+    import nfc
+    import time
+    import nfc.ndef
+    import nfc.tag
+    import nfc.clf
+
+    _cardFrontend=nfc.ContactlessFrontend('i2c:0x24:pn532')
+    _nextTimeout=0
+
+    def _on_connect(tag):
+      return 
 
 
-import nfc
-import time
-import nfc.ndef
-import nfc.tag
-import nfc.clf
 
-_cardFrontend=nfc.ContactlessFrontend('i2c:0x24:pn532')
-_nextTimeout=0
+    def waitForTag(timeout):
+      """ Wait for an NFC tag (e.g. a university card) and return the
+          ID of the tag.
 
-def _on_connect(tag):
-  return 
+         Args: 
+            timeout:
+               Time in seconds to wait for a tag
+         
+         Returns:
+            List containing each byte of the tag ID, or None if no tag found 
+      """
+      global _nextTimeout
+      _nextTimeout=time.time()+timeout
+      tagObj=_cardFrontend.connect(rdwr={'on-connect':_on_connect},terminate=isTimeOut)
+      if tagObj is None:
+        return None
+      id=[]
+      for c in tagObj.identifier:
+        id.append(c)
+      return id
+      
 
+    def isTimeOut():
+      global _nextTimeout
+      if time.time()>_nextTimeout:
+        return True
+      return False
 
-
-def waitForTag(timeout):
-  """ Wait for an NFC tag (e.g. a university card) and return the
-      ID of the tag.
-
-     Args: 
-        timeout:
-           Time in seconds to wait for a tag
-     
-     Returns:
-        List containing each byte of the tag ID, or None if no tag found 
-  """
-  global _nextTimeout
-  _nextTimeout=time.time()+timeout
-  tagObj=_cardFrontend.connect(rdwr={'on-connect':_on_connect},terminate=isTimeOut)
-  if tagObj is None:
-    return None
-  id=[]
-  for c in tagObj.identifier:
-    id.append(c)
-  return id
-  
-
-def isTimeOut():
-  global _nextTimeout
-  if time.time()>_nextTimeout:
-    return True
-  return False
-
+else:
+    import pn532
+    
+    def waitForTag(timeout):
+        reader=pn532.PN532()
+        reader.begin()
+        reader.SAM_configuration()
+        return reader.read_passive_target(timeout_sec=timeout)
+      
+      
 if __name__=="__main__":
   print(waitForTag(200))
 
